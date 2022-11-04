289d9c2adafd442c3c2b0e9b7e27c618688be856
delouxeon <oluxnwabuife@gmail.com>
2022-11-04 17:18:18 +0100

    Fix: A bug fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import * as React from 'react';
 import './App.css';
 
-import {
-  BrowseRouter as Router,
-  Switch,
-  Route,
-  Redirect,
-} from "react-router-dom";
+// import {
+//   BrowseRouter as Router,
+//   Switch,
+//   Route,
+//   Redirect,
+// } from "react-router-dom";
 
 
 import Home from "./Indexpage/index"
