436c445c3b6d2aadcd9f6d66d2fc136d8b596b88
delouxeon <oluxnwabuife@gmail.com>
2022-11-05 14:40:23 +0100

    Refactor:classname added to contact page header

--- src/contactpage/contact-header.jsx
+++ src/contactpage/contact-header.jsx
@@ -3,7 +3,7 @@
 const ContactHeader = () => {
 
 return(
-  <header>
+  <header className="ContactPage-header">
     <h1 className="contactMe-header">Contact Me</h1>
     <p className="header-message">Hi there, contact me to ask me about anything you have in mind.</p>
   </header>
